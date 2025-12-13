1dd00257b2cc58a9e6f1e776f131b319bda5b6b3
Aaron L <aaron.lb.dev@gmail.com>
2025-12-13 11:33:11 +0100

    Implement async data fetching in Home component to display popular movies from TMDB API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getPopularMovies } from '@/lib/api/tmdb/movies';
+export default async function Home() {
+  const movies: any = await getPopularMovies();
+
   return (
     <div>
       <h1>Hello World</h1>
+      <ul>
+        {movies.results.map((movie: any) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
