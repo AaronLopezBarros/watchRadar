2711bb97a41e2f3475d5ad9e2400abb13ca365b7
Aaron L <aaron.lb.dev@gmail.com>
2025-12-26 10:39:16 +0100

    feat: Update layout and Home component to enhance styling and display movie posters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
+import Image from 'next/image';
+
 import { getPopularMoviesMultiplePages } from '@/src/lib/api/tmdb/movies';
 import { Movie } from '@/src/lib/api/tmdb/types';
+import { getPosterUrl } from '@/src/lib/utils/getPosterUrl';
 
 export default async function Home() {
   const movies = await getPopularMoviesMultiplePages(3);
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <ul>
-        {movies.map((movie: Movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        ))}
-      </ul>
+    <div className='wrap flex items-center justify-center gap-3'>
+      {movies.map((movie: Movie) => (
+        <Image key={movie.id} src={getPosterUrl(movie.poster_path)} alt={movie.title} width={100} height={100} />
+      ))}
     </div>
   );
 }
