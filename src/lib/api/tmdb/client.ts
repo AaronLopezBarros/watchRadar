e4a3956cb9465fced550f39f125552d1bfc1798f
Aaron L <aaron.lb.dev@gmail.com>
2026-06-13 10:14:30 +0200

    feat: Enhance tmdbClient to support query parameters for API requests

--- src/lib/api/tmdb/client.ts
+++ src/lib/api/tmdb/client.ts
@@ -1,11 +1,14 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+type QueryParams = Record<string, string | number | boolean | undefined | null>;
+
 type TmdbClientOptions = RequestInit & {
   revalidate?: number;
+  params?: QueryParams;
 };
 
 export const tmdbClient = async <T>(endpoint: string, options?: TmdbClientOptions): Promise<T> => {
-  const { revalidate = 3600, headers = {}, ...restOptions } = options || {};
+  const { params, revalidate = 3600, headers = {}, ...restOptions } = options || {};
 
   const token = process.env.TMDB_TOKEN || process.env.TMDB_API_KEY;
 
@@ -13,7 +16,15 @@ export const tmdbClient = async <T>(endpoint: string, options?: TmdbClientOption
     throw new Error('TMDB_TOKEN o TMDB_API_KEY is not set in the environment variables');
   }
 
-  const url = `${BASE_URL}${endpoint}`;
+  const searchParams = new URLSearchParams();
+
+  Object.entries(params || {}).forEach(([key, value]) => {
+    if (value !== undefined && value !== null) {
+      searchParams.append(key, String(value));
+    }
+  });
+
+  const url = `${BASE_URL}${endpoint}` + (searchParams.size ? `?${searchParams.toString()}` : '');
 
   const response = await fetch(url, {
     ...restOptions,
@@ -28,7 +39,7 @@ export const tmdbClient = async <T>(endpoint: string, options?: TmdbClientOption
   if (!response.ok) {
     const errorMessage = `Error ${response.status}: ${response.statusText}`;
 
-    throw new Error(`TMDB API Error: ${errorMessage}`);
+    throw new Error(`TMDB API Error ${endpoint}: ${errorMessage}`);
   }
 
   return response.json();
